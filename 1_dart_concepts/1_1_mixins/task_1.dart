52f509ea460bee6a13ff8fda9d4a3328456c306d
Tolandorado <tonisagaz@gmail.com>
2025-12-06 15:59:00 +0300

    feat(mixins): complete task_1

--- 1_dart_concepts/1_1_mixins/task_1.dart
+++ 1_dart_concepts/1_1_mixins/task_1.dart
@@ -1,4 +1,21 @@
+extension DateTimeToString on DateTime {
+  String toFormattedString() {
+    final y = year.toString();
+    final m = month.toString().padLeft(2, '0');
+    final d = day.toString().padLeft(2, '0');
+    
+
+    final h = hour.toString().padLeft(2, '0');
+    final min = minute.toString().padLeft(2, '0');
+    final s = second.toString().padLeft(2, '0');
+    
+    return "$y.$m.$d $h:$min:$s";
+  }
+}
+
 void main() {
   // Implement an extension on [DateTime], returning a [String] in format of
   // `YYYY.MM.DD hh:mm:ss` (e.g. `2023.01.01 00:00:00`).
+  final date = DateTime.now();  
+  print(date.toFormattedString());
 }
