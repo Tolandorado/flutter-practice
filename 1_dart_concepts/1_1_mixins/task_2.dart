4fdd61d6ed0a87c898efd23ec6f49ef332a28ec9
Tolandorado <tonisagaz@gmail.com>
2025-12-10 17:13:57 +0300

    Completed 1_1_mixins

--- 1_dart_concepts/1_1_mixins/task_2.dart
+++ 1_dart_concepts/1_1_mixins/task_2.dart
@@ -1,7 +1,50 @@
+extension ParseLink on String {
+  List<dynamic> parseLink() {
+    final linkRegex = RegExp(
+      r'(https?:\/\/[^\s]+|[\w\.-]+\.[a-z]{2,})',
+      caseSensitive: false,
+    );
+    final matches = linkRegex.allMatches(this);
+    List<dynamic> result = [];
+    int lastIndex = 0;
+
+    for (final match in matches) {
+      if (match.start > lastIndex) {
+        result.add(TextPart(this.substring(lastIndex, match.start)));
+      }
+      result.add(LinkPart(match.group(0)!));
+      lastIndex = match.end;
+    }
+
+    if (lastIndex < this.length) {
+      result.add(TextPart(this.substring(lastIndex)));
+    }
+    return result;
+  }
+}
+
+class TextPart {
+  final String text;
+  TextPart(this.text);
+
+  @override
+  String toString() => "Text($text)";
+}
+
+class LinkPart {
+  final String link;
+  LinkPart(this.link);
+
+  @override
+  String toString() => "Link($link)";
+}
+
 void main() {
   // Implement an extension on [String], parsing links from a text.
   //
   // Extension should return a [List] of texts and links, e.g.:
   // - `Hello, google.com, yay` ->
   //   [Text('Hello, '), Link('google.com'), Text(', yay')].
+  String str = 'Hello, google.com, yay';
+  print(str.parseLink());
 }
--- 1_dart_concepts/1_1_mixins/task_3.dart
+++ 1_dart_concepts/1_1_mixins/task_3.dart
@@ -1,6 +1,24 @@
 /// Object equipable by a [Character].
 abstract class Item {}
 
+mixin HasDamage {
+  late int _damage;
+
+  int get getDamage => _damage;
+}
+
+mixin HasDefence {
+  late int _defence;
+
+  int get getDefence => _defence;
+}
+
+mixin EquipableHands {}
+mixin EquipableHat {}
+mixin EquipableTorso {}
+mixin EquipableLegs {}
+mixin EquipableShoes {}
+
 /// Entity equipping [Item]s.
 class Character {
   Item? leftHand;
@@ -16,26 +34,91 @@ class Character {
 
   /// Returns the total damage of this [Character].
   int get damage {
-    // TODO: Implement me.
-    return 0;
+    return equipped.whereType<HasDamage>().fold(
+      0,
+      (sum, item) => sum + item.getDamage,
+    );
   }
 
   /// Returns the total defense of this [Character].
   int get defense {
-    // TODO: Implement me.
-    return 0;
+    return equipped.whereType<HasDefence>().fold(
+      0,
+      (sum, item) => sum + item.getDefence,
+    );
   }
 
   /// Equips the provided [item], meaning putting it to the corresponding slot.
   ///
   /// If there's already a slot occupied, then throws a [OverflowException].
   void equip(Item item) {
-    // TODO: Implement me.
+    switch (item) {
+      case EquipableHands _:
+        leftHand ??= item;
+        if (leftHand == item) {
+          break;
+        }
+        rightHand ??= item;
+        if (rightHand == item) {
+          break;
+        }
+        throw OverflowException();
+      case EquipableHat _:
+        hat ??= item;
+        if (hat != item) {
+          throw OverflowException();
+        }
+        break;
+      case EquipableTorso _:
+        torso ??= item;
+        if (torso != item) {
+          throw OverflowException();
+        }
+        break;
+      case EquipableLegs _:
+        legs ??= item;
+        if (legs != item) {
+          throw OverflowException();
+        }
+        break;
+      case EquipableShoes _:
+        shoes ??= item;
+        if (shoes != item) {
+          throw OverflowException();
+        }
+        break;
+      default:
+        throw ArgumentError('Unsupported item type');
+    }
   }
 }
 
 /// [Exception] indicating there's no place left in the [Character]'s slot.
 class OverflowException implements Exception {}
+
+class Sword extends Item with HasDamage, EquipableHands {
+  Sword(int damage) {
+    this._damage = damage;
+  }
+}
+
+class Spear extends Item with HasDamage, EquipableHands {
+  Spear(int damage) {
+    this._damage = damage;
+  }
+}
+
+class Hat extends Item with HasDefence, EquipableHat {
+  Hat(int defence) {
+    this._defence = defence;
+  }
+}
+
+class Torso extends Item with HasDefence, EquipableTorso {
+  Torso(int defence) {
+    this._defence = defence;
+  }
+}
 
 void main() {
   // Implement mixins to differentiate [Item]s into separate categories to be
@@ -43,4 +126,12 @@ void main() {
   // armor should have some defense property.
   //
   // [Character] can equip weapons into hands, helmets onto hat, etc.
+  Character character = Character();
+  character.equip(Sword(20));
+  character.equip(Spear(10));
+  character.equip(Hat(15));
+  character.equip(Torso(20));
+
+  print('Total damage: ${character.damage}');
+  print('Total defence: ${character.defense}');
 }
