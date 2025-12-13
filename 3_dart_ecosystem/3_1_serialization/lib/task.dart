58523e4334126188e0e471455f9d84b227669bfc
Tolandorado <tonisagaz@gmail.com>
2025-12-13 17:11:40 +0300

    Complete 3_1_serialization

---
+++ 3_dart_ecosystem/3_1_serialization/lib/task.dart
@@ -0,0 +1,173 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:toml/toml.dart';
+import 'package:json2yaml/json2yaml.dart';
+
+part 'task.g.dart';
+
+// Конвертер для Duration
+class DurationConverter implements JsonConverter<Duration, String> {
+  const DurationConverter();
+
+  @override
+  Duration fromJson(String json) {
+    if (json.endsWith('h')) {
+      return Duration(hours: int.parse(json.replaceAll('h', '')));
+    } else if (json.endsWith('m')) {
+      return Duration(minutes: int.parse(json.replaceAll('m', '')));
+    } else if (json.endsWith('ms')) {
+      return Duration(milliseconds: int.parse(json.replaceAll('ms', '')));
+    }
+    return Duration.zero;
+  }
+
+  @override
+  String toJson(Duration object) {
+    if (object.inHours > 0) {
+      return '${object.inHours}h';
+    } else if (object.inMinutes > 0) {
+      return '${object.inMinutes}m';
+    } else {
+      return '${object.inMilliseconds}ms';
+    }
+  }
+}
+
+@JsonSerializable(explicitToJson: true)
+class Request {
+  final String type;
+  final StreamInfo stream;
+  final List<Gift> gifts;
+  final DebugInfo debug;
+
+  Request({
+    required this.type,
+    required this.stream,
+    required this.gifts,
+    required this.debug,
+  });
+
+  factory Request.fromJson(Map<String, dynamic> json) =>
+      _$RequestFromJson(json);
+  Map<String, dynamic> toJson() => _$RequestToJson(this);
+}
+
+@JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+class StreamInfo {
+  final String userId;
+  final bool isPrivate;
+  final int settings;
+  final String shardUrl;
+  final PublicTariff publicTariff;
+  final PrivateTariff privateTariff;
+
+  StreamInfo({
+    required this.userId,
+    required this.isPrivate,
+    required this.settings,
+    required this.shardUrl,
+    required this.publicTariff,
+    required this.privateTariff,
+  });
+
+  factory StreamInfo.fromJson(Map<String, dynamic> json) =>
+      _$StreamInfoFromJson(json);
+  Map<String, dynamic> toJson() => _$StreamInfoToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class PublicTariff {
+  final int id;
+  final int price;
+  @DurationConverter()
+  final Duration duration;
+  final String description;
+
+  PublicTariff({
+    required this.id,
+    required this.price,
+    required this.duration,
+    required this.description,
+  });
+
+  factory PublicTariff.fromJson(Map<String, dynamic> json) =>
+      _$PublicTariffFromJson(json);
+  Map<String, dynamic> toJson() => _$PublicTariffToJson(this);
+}
+
+@JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+class PrivateTariff {
+  final int clientPrice;
+  @DurationConverter()
+  final Duration duration;
+  final String description;
+
+  PrivateTariff({
+    required this.clientPrice,
+    required this.duration,
+    required this.description,
+  });
+
+  factory PrivateTariff.fromJson(Map<String, dynamic> json) =>
+      _$PrivateTariffFromJson(json);
+  Map<String, dynamic> toJson() => _$PrivateTariffToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class Gift {
+  final int id;
+  final int price;
+  final String description;
+
+  Gift({
+    required this.id,
+    required this.price,
+    required this.description,
+  });
+
+  factory Gift.fromJson(Map<String, dynamic> json) => _$GiftFromJson(json);
+  Map<String, dynamic> toJson() => _$GiftToJson(this);
+}
+
+@JsonSerializable(explicitToJson: true)
+class DebugInfo {
+  @DurationConverter()
+  final Duration duration;
+  final DateTime at;
+
+  DebugInfo({
+    required this.duration,
+    required this.at,
+  });
+
+  factory DebugInfo.fromJson(Map<String, dynamic> json) =>
+      _$DebugInfoFromJson(json);
+  Map<String, dynamic> toJson() => _$DebugInfoToJson(this);
+}
+
+void main() {
+  // Write a program which deserializes the `request.json` into a `Request`
+  // class and prints out its serialization in YAML and TOML formats.
+  // Consider to choose correct and accurate types for data representation.
+  final file = File('request.json');
+  if (!file.existsSync()) {
+    print(
+        'Ошибка: файл request.json не найден. Убедитесь, что он находится в корневой папке проекта.');
+    return;
+  }
+
+  final jsonString = file.readAsStringSync();
+  final jsonMap = jsonDecode(jsonString) as Map<String, dynamic>;
+  final request = Request.fromJson(jsonMap);
+
+
+  final requestMap = request.toJson();
+  final yamlString = json2yaml(requestMap);
+  final tomlString = TomlDocument.fromMap(requestMap);
+
+  print('--- YAML ---');
+  print(yamlString);
+  print('--- TOML ---');
+  print(tomlString);
+}
---
+++ 3_dart_ecosystem/3_1_serialization/lib/task.g.dart
@@ -0,0 +1,95 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'task.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Request _$RequestFromJson(Map<String, dynamic> json) => Request(
+      type: json['type'] as String,
+      stream: StreamInfo.fromJson(json['stream'] as Map<String, dynamic>),
+      gifts: (json['gifts'] as List<dynamic>)
+          .map((e) => Gift.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      debug: DebugInfo.fromJson(json['debug'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$RequestToJson(Request instance) => <String, dynamic>{
+      'type': instance.type,
+      'stream': instance.stream.toJson(),
+      'gifts': instance.gifts.map((e) => e.toJson()).toList(),
+      'debug': instance.debug.toJson(),
+    };
+
+StreamInfo _$StreamInfoFromJson(Map<String, dynamic> json) => StreamInfo(
+      userId: json['user_id'] as String,
+      isPrivate: json['is_private'] as bool,
+      settings: (json['settings'] as num).toInt(),
+      shardUrl: json['shard_url'] as String,
+      publicTariff:
+          PublicTariff.fromJson(json['public_tariff'] as Map<String, dynamic>),
+      privateTariff: PrivateTariff.fromJson(
+          json['private_tariff'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$StreamInfoToJson(StreamInfo instance) =>
+    <String, dynamic>{
+      'user_id': instance.userId,
+      'is_private': instance.isPrivate,
+      'settings': instance.settings,
+      'shard_url': instance.shardUrl,
+      'public_tariff': instance.publicTariff.toJson(),
+      'private_tariff': instance.privateTariff.toJson(),
+    };
+
+PublicTariff _$PublicTariffFromJson(Map<String, dynamic> json) => PublicTariff(
+      id: (json['id'] as num).toInt(),
+      price: (json['price'] as num).toInt(),
+      duration: const DurationConverter().fromJson(json['duration'] as String),
+      description: json['description'] as String,
+    );
+
+Map<String, dynamic> _$PublicTariffToJson(PublicTariff instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'price': instance.price,
+      'duration': const DurationConverter().toJson(instance.duration),
+      'description': instance.description,
+    };
+
+PrivateTariff _$PrivateTariffFromJson(Map<String, dynamic> json) =>
+    PrivateTariff(
+      clientPrice: (json['client_price'] as num).toInt(),
+      duration: const DurationConverter().fromJson(json['duration'] as String),
+      description: json['description'] as String,
+    );
+
+Map<String, dynamic> _$PrivateTariffToJson(PrivateTariff instance) =>
+    <String, dynamic>{
+      'client_price': instance.clientPrice,
+      'duration': const DurationConverter().toJson(instance.duration),
+      'description': instance.description,
+    };
+
+Gift _$GiftFromJson(Map<String, dynamic> json) => Gift(
+      id: (json['id'] as num).toInt(),
+      price: (json['price'] as num).toInt(),
+      description: json['description'] as String,
+    );
+
+Map<String, dynamic> _$GiftToJson(Gift instance) => <String, dynamic>{
+      'id': instance.id,
+      'price': instance.price,
+      'description': instance.description,
+    };
+
+DebugInfo _$DebugInfoFromJson(Map<String, dynamic> json) => DebugInfo(
+      duration: const DurationConverter().fromJson(json['duration'] as String),
+      at: DateTime.parse(json['at'] as String),
+    );
+
+Map<String, dynamic> _$DebugInfoToJson(DebugInfo instance) => <String, dynamic>{
+      'duration': const DurationConverter().toJson(instance.duration),
+      'at': instance.at.toIso8601String(),
+    };
