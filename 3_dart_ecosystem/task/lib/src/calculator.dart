ba24e818e1fdc09f32efdcd28090e4a1c9587c07
Tolandorado <tonisagaz@gmail.com>
2025-12-16 15:20:26 +0300

    Complete dart ecosystem task

---
+++ 3_dart_ecosystem/task/lib/src/calculator.dart
@@ -0,0 +1,9 @@
+class Calculator {
+  double sum(double a, double b) => a + b;
+
+  double subtract(double a, double b) => a - b;
+
+  double multiply(double a, double b) => a * b;
+
+  double divide(double a, double b) => a / b;
+}
---
+++ 3_dart_ecosystem/task/lib/task.dart
@@ -0,0 +1,9 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+// export 'src/task_base.dart';
+export 'src/calculator.dart';
+
+// TODO: Export any libraries intended for clients of this package.
